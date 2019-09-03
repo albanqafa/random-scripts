78a535238faaf37dba9fdaf35d9665927d1ded52
Alban Q <37601993+albanqafa@users.noreply.github.com>
2019-09-03 19:09:29 -0400

    removed all special chars cause... issues

--- nowplaying.sh
+++ nowplaying.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 publishdir='/volume1/web/www/nowplaying/test.html'
 do_repeat () {
-querykodi=$(curl -s --header 'Content-Type: application/json' --data-binary '{ "id": 1, "jsonrpc": "2.0", "method": "Player.GetItem", "params": { "properties": [ "title", "showtitle" ], "playerid": 1 }, "id": "VideoGetItem" }' 'http://192.168.1.123:8080/jsonrpc' | cut -d: -f6-9 | cut -d'"' -f8,12 | tr '"' ' ' | tr -d '\n')
+querykodi=$(curl -s --header 'Content-Type: application/json' --data-binary '{ "id": 1, "jsonrpc": "2.0", "method": "Player.GetItem", "params": { "properties": [ "title", "showtitle" ], "playerid": 1 }, "id": "VideoGetItem" }' 'http://192.168.1.123:8080/jsonrpc' | cut -d: -f6-9 | cut -d'"' -f8,12 | tr '"' ' ' | tr -d '\n'  | sed 's/[\._-]//g')
 if [[ ${querykodi%%[[:space:]]##[[:space:]]} == 'type' ]];
 then
 	querykodi=$(echo "Nothings Playing")
-	$(echo '<h2 id="nowplaying" style="color:#32DD72; font-size:3vw; font-family:lain;">'$querykodi'</h2>' > $publishdir)
+	$(echo '<h1 id="nowplaying" style="font-size:3vw; font-family:lain;">'$querykodi'</h1>' > $publishdir)
 else
-	$(echo '<h2 id="nowplaying" style="color:#32DD72; font-size:3vw; font-family:lain;">'${querykodi%%[[:space:]]##[[:space:]]}'</h2>' > $publishdir)
+	$(echo '<h1 id="nowplaying" style="font-size:3vw; font-family:lain;">'${querykodi%%[[:space:]]##[[:space:]]}'</h1>' > $publishdir)
 fi
 }
 while true
