ecc2d9bf92c73975288faee43a75121e7b27f812
Alban Q <37601993+albanqafa@users.noreply.github.com>
2022-02-18 10:24:41 -0500

    Update Rehmann_AD_User_Audit.ps1

--- Rehmann_AD_User_Audit.ps1
+++ Rehmann_AD_User_Audit.ps1
@@ -1,12 +1,13 @@
-Get-ADUser -Filter 'enabled -eq $true' -Properties SamAccountname,DisplayName,PasswordNeverExpires,CannotChangePassword,AccountExpirationDate,pwdLastSet,LogonHours,memberof | % {
+Get-ADUser -Filter * -Properties SamAccountname,DisplayName,Enabled,PasswordNeverExpires,CannotChangePassword,accountExpires,pwdLastSet,LogonHours,memberof | % {
 New-Object PSObject -Property @{
 DisplayName= $_.DisplayName
 SamAccountname= $_.SamAccountname
+Enabled= $_.Enabled
 PasswordNeverExpires= $_.PasswordNeverExpires
 CannotChangePassword= $_.CannotChangePassword
-AccountDoesNotExpire= ($null -eq $_.AccountExpirationDate)
-ChangePasswordAtNextLogon= ($pwdLastSet -eq 0)
+AccountDoesNotExpire= ($_.accountExpires -eq 0)
+ChangePasswordAtNextLogon= ($_.pwdLastSet -eq 0)
 LogonHours= $_.LogonHours
 Groups = ($_.memberof | Get-ADGroup | Select -ExpandProperty Name) -join 
 ","}
-} | Select SamAccountname,DisplayName,PasswordNeverExpires,CannotChangePassword,AccountDoesNotExpire,ChangePasswordAtNextLogon,LogonHours,Groups | format-table -autosize | Out-String -Width 10000 | out-file ./test.txt
+} | Select SamAccountname,DisplayName,Enabled,PasswordNeverExpires,CannotChangePassword,AccountDoesNotExpire,ChangePasswordAtNextLogon,LogonHours,Groups | format-table -autosize | Out-String -Width 10000 | out-file ./test.txt
